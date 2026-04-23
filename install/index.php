68d226a25f51f844be489103b5c8dde71707a2c7
Evgeniy <51183910+likemne@users.noreply.github.com>
2026-04-23 16:06:51 +0700

    Update index.php

--- install/index.php
+++ install/index.php
@@ -1,30 +1,6 @@
-public function DoInstall()
-{
-    if ($this->isAlreadyInstalled()) {
-        $this->errors[] = Loc::getMessage("TOLORRIC_MODULE_ALREADY_INSTALLED");
-        return false;
-    }
-    
-    ModuleManager::registerModule($this->MODULE_ID);
-    
-    // Создаём таблицу в БД
-    $this->installDB();
-    
-    return true;
-}
-
-public function installDB()
-{
-    $connection = \Bitrix\Main\Application::getConnection();
-    if (!$connection->isTableExists(\Tolorric\Likemne\NoteTable::getTableName())) {
-        \Tolorric\Likemne\NoteTable::getEntity()->createDbTable();
-    }
-}
-
-public function uninstallDB()
-{
-    $connection = \Bitrix\Main\Application::getConnection();
-    if ($connection->isTableExists(\Tolorric\Likemne\NoteTable::getTableName())) {
-        $connection->dropTable(\Tolorric\Likemne\NoteTable::getTableName());
-    }
-}
+<?php
+$MESS["TOLORRIC_MODULE_NAME"] = "Мой первый модуль Tolorric";
+$MESS["TOLORRIC_MODULE_DESCRIPTION"] = "Мастер-модуль, который учится стандартам Битрикс";
+$MESS["TOLORRIC_PARTNER_NAME"] = "Моё Имя";
+$MESS["TOLORRIC_PARTNER_URI"] = "https://example.com";
+$MESS["TOLORRIC_MODULE_ALREADY_INSTALLED"] = "Модуль уже установлен";
