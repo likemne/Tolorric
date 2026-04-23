db6a62dbbb48a28c1982cecfb6f00d34b25f3b1e
Evgeniy <51183910+likemne@users.noreply.github.com>
2026-04-23 15:56:06 +0700

    Update include.php

--- include.php
+++ include.php
@@ -5,7 +5,6 @@ use Bitrix\Main\Loader;
 Loader::registerAutoLoadClasses(
     'tolorric.likemne',
     [
-        // Здесь будут классы твоего модуля:
-        // 'Tolorric\Likemne\SomeClass' => 'lib/someclass.php',
+        'Tolorric\\Likemne\\NoteTable' => 'lib/NoteTable.php',
     ]
 );
