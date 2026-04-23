b8cb9bac002888290cdb351f055c2647552a9bd1
Evgeniy <51183910+likemne@users.noreply.github.com>
2026-04-23 16:07:31 +0700

    Update index.php

--- install/index.php
+++ install/index.php
@@ -1,6 +1,66 @@
 <?php
-$MESS["TOLORRIC_MODULE_NAME"] = "Мой первый модуль Tolorric";
-$MESS["TOLORRIC_MODULE_DESCRIPTION"] = "Мастер-модуль, который учится стандартам Битрикс";
-$MESS["TOLORRIC_PARTNER_NAME"] = "Моё Имя";
-$MESS["TOLORRIC_PARTNER_URI"] = "https://example.com";
-$MESS["TOLORRIC_MODULE_ALREADY_INSTALLED"] = "Модуль уже установлен";
+
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\ModuleManager;
+
+Loc::loadMessages(__FILE__);
+
+class tolorric_likemne extends CModule
+{
+    public $MODULE_ID = "tolorric.likemne";
+    public $MODULE_VERSION;
+    public $MODULE_VERSION_DATE;
+    public $MODULE_NAME;
+    public $MODULE_DESCRIPTION;
+    public $PARTNER_NAME;
+    public $PARTNER_URI;
+
+    public $errors = [];
+
+    public function __construct()
+    {
+        $arModuleVersion = [];
+        include(__DIR__ . "/version.php");
+
+        $this->MODULE_VERSION = $arModuleVersion["VERSION"];
+        $this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
+        $this->MODULE_NAME = Loc::getMessage("TOLORRIC_MODULE_NAME");
+        $this->MODULE_DESCRIPTION = Loc::getMessage("TOLORRIC_MODULE_DESCRIPTION");
+        $this->PARTNER_NAME = Loc::getMessage("TOLORRIC_PARTNER_NAME");
+        $this->PARTNER_URI = Loc::getMessage("TOLORRIC_PARTNER_URI");
+    }
+
+    public function DoInstall()
+    {
+        if ($this->isAlreadyInstalled()) {
+            $this->errors[] = Loc::getMessage("TOLORRIC_MODULE_ALREADY_INSTALLED");
+            return false;
+        }
+
+        ModuleManager::registerModule($this->MODULE_ID);
+        $this->installDB();
+        return true;
+    }
+
+    public function DoUninstall()
+    {
+        $this->uninstallDB();
+        ModuleManager::unRegisterModule($this->MODULE_ID);
+        return true;
+    }
+
+    private function isAlreadyInstalled(): bool
+    {
+        return ModuleManager::isModuleInstalled($this->MODULE_ID);
+    }
+
+    public function installDB()
+    {
+        return;
+    }
+
+    public function uninstallDB()
+    {
+        return;
+    }
+}
