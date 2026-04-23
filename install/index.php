98fa97d02d1f72b73c3786e5204ad28b0577744b
Evgeniy <51183910+likemne@users.noreply.github.com>
2026-04-23 15:49:43 +0700

    Update index.php

--- install/index.php
+++ install/index.php
@@ -1,10 +1,8 @@
 <?php
-// /local/modules/tolorric.likemne/install/index.php
 
 use Bitrix\Main\Localization\Loc;
 use Bitrix\Main\ModuleManager;
 
-// Загружаем языковые файлы
 Loc::loadMessages(__FILE__);
 
 class tolorric_likemne extends CModule
@@ -17,7 +15,6 @@ class tolorric_likemne extends CModule
     public $PARTNER_NAME;
     public $PARTNER_URI;
 
-    // Свойство для хранения ошибок установки
     public $errors = [];
 
     public function __construct()
@@ -35,34 +32,23 @@ class tolorric_likemne extends CModule
 
     public function DoInstall()
     {
-        // Проверяем, не установлен ли модуль
         if ($this->isAlreadyInstalled()) {
             $this->errors[] = Loc::getMessage("TOLORRIC_MODULE_ALREADY_INSTALLED");
             return false;
         }
         
-        // Регистрируем модуль в системе
         ModuleManager::registerModule($this->MODULE_ID);
-        
-        // Здесь в будущем будем копировать файлы или создавать таблицы БД
-        
         return true;
     }
 
     public function DoUninstall()
     {
-        // Снимаем регистрацию модуля
         ModuleManager::unRegisterModule($this->MODULE_ID);
-        
-        // Здесь в будущем будем удалять файлы или таблицы БД
-        
         return true;
     }
 
-    // Проверка, не установлен ли уже модуль
     private function isAlreadyInstalled(): bool
     {
         return ModuleManager::isModuleInstalled($this->MODULE_ID);
     }
-
 }
